7ec57702e88ff4a29c7b9ffa652e0394e32ff720
Catriona <catriona@hashicorp.com>
2023-09-20 10:25:33 +0100

    remove ids

--- storage/2020-08-04/file/directories/resource_id.go
+++
@@ -1,6 +0,0 @@
-package directories
-
-// GetResourceID returns the Resource ID for the given Directory
-// This can be useful when, for example, you're using this as a unique identifier
-
-// TODO find out what the resource manager id is
--- storage/2020-08-04/file/files/resource_id.go
+++
@@ -1,5 +0,0 @@
-package files
-
-// GetResourceID returns the Resource ID for the given File
-// This can be useful when, for example, you're using this as a unique identifier
-// TODO find out resource manager id
